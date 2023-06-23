c2b7e798085c4c3e312748f1bdf32f5d236787ff
Eduardo Gonzalez <eduardo_gzz@outlook.com>
2023-06-22 18:11:25 -0600

    robustecer dataset

--- agregaciones.sql
+++ agregaciones.sql
@@ -1,7 +1,7 @@
 -- Conteo de frecuencias de posiciones de jugadores en un equipo específico (por ejemplo, el equipo con ID 1)
 SELECT posicion, COUNT(*) AS frecuencia
 FROM jugadores
-WHERE equipo = 1
+--WHERE equipo = 1
 GROUP BY posicion;
 
 -- Cálculo de la media de la capacidad de los estadios
@@ -12,15 +12,24 @@ FROM estadios;
 SELECT MIN(puntos_local) AS min_puntos, MAX(puntos_local) AS max_puntos
 FROM resultados;
 
--- Equipo con la capacidad mínima y máxima de estadios
+-- Capacidad mínima y máxima de estadios
 SELECT MIN(capacidad) AS min_capacidad, MAX(capacidad) AS max_capacidad
 FROM estadios;
+
+--Estadio con la capacidad mínima
+SELECT estadios.nombre
+FROM estadios
+WHERE capacidad = (SELECT MIN(capacidad) FROM estadios);
 
 -- Cálculo del primer cuartil (25%) de la capacidad de los estadios
 SELECT PERCENTILE_CONT(0.25) WITHIN GROUP (ORDER BY capacidad) AS primer_cuartil
 FROM estadios
-WHERE capacidad IS NOT NULL
-HAVING primer_cuartil != MEDIAN(capacidad);
+WHERE capacidad IS NOT NULL;
+
+-- Cálculo de la moda de las posiciones de los jugadores en un equipo específico (por ejemplo, el equipo con ID 1)
+SELECT MODE() WITHIN GROUP (ORDER BY posicion) AS moda, COUNT(*)
+FROM jugadores
+WHERE equipo = 1;
 
 -- Cálculo de la moda de las posiciones de los jugadores en un equipo específico (por ejemplo, el equipo con ID 1)
 SELECT posicion, COUNT(*) AS frecuencia
